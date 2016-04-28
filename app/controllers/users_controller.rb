e7580128a8e4cca880d507df8d05d3d342ab1956
Patchx <robert.anderson.fl@gmail.com>
2016-04-27 20:54:59 -0400

    users have a create lesson button, and only the first user can see all users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,15 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!
+  before_action :authenticate_user!, only: [:index]
 
   def index
+    unless current_user.id == 1
+      redirect_to page_path('denied')
+    end
     @users = User.all
   end
 
   def show
     @user = User.find(params[:id])
-    unless @user == current_user
-      redirect_to :back, :alert => "Access denied."
-    end
   end
 
 end
